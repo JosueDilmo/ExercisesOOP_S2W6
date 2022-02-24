9aa4099db26ebabb836deadb66f415e95b8126b7
josuedilmo <josuedilmo@gmail.com>
2022-02-24 15:53:48 +0000

    Add project files.

---
+++ InClass/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercisesOOP_S2W6.InClass
+{
+    class Person
+    {
+        // just class to practice some in class exercises
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using ExercisesOOP_S2W6.InClass;
+using System;
+
+namespace ExercisesOOP_S2W6
+{
+
+    class Program {      
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercises S2W6");
+            Console.WriteLine("Question .1");
+            Console.WriteLine("Question .2");
+            Console.WriteLine("Question .3");
+            Console.Write("Choose a question: ");
+            int menuOpt = Convert.ToInt32(Console.ReadLine());
+            switch (menuOpt)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+
+            }
+        }
+    }
+}
